3e5c7b715232304dce7eeccf4feaee8750a75bf0
siegfredlorelle <siegfredlorelle09@gmail.com>
2023-11-01 21:18:02 +0800

    Always show an input of 0, do not display left 0s

--- js/script.js
+++ js/script.js
@@ -2,23 +2,35 @@ memoryDisplay = document.querySelector(".memory-display > span");
 mainDisplay = document.querySelector(".main-display > span");
 numBtns = document.querySelectorAll(".num-btn");
 allClearBtn = document.querySelector(".ac-btn");
+backBtn = document.querySelector(".back-btn");
 
-console.log(numBtns);
-console.log(memoryDisplay);
-console.log(mainDisplay);
-memoryDisplay.text = "";
-mainDisplay.text = "";
+// console.log(numBtns);
+// console.log(memoryDisplay);
+// console.log(mainDisplay);
+
+memoryDisplay.text = "0";
+mainDisplay.text = "0";
 
 numBtns.forEach(btn => {
   btn.addEventListener("click", () => {
     console.log(btn.textContent);
-    mainDisplay.textContent += btn.textContent;
+    if (mainDisplay.textContent == "0") {
+      mainDisplay.textContent = btn.textContent;
+    }
+    else {
+      mainDisplay.textContent += btn.textContent;
+    }
   })
 });
 
 allClearBtn.addEventListener("click", () => {
-  mainDisplay.textContent = "";
+  mainDisplay.textContent = "0";
 });
 
-
+backBtn.addEventListener("click", () => {
+  mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+  if (!mainDisplay.textContent) {
+    mainDisplay.textContent = 0;
+  }
+});
 
