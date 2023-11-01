0199b8b589842b516fbc537439887bef5f2ab50f
siegfredlorelle <siegfredlorelle09@gmail.com>
2023-11-01 22:13:38 +0800

    Limit display to a maximum of one decimal point

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
+const resetAll = () => {
+  mainDisplay.textContent = "0";
+  hasDeciPoint = false;
+  console.log("resetting");
+  // TODO: Fetch current memory
+} 
+
+
 memoryDisplay = document.querySelector(".memory-display > span");
 mainDisplay = document.querySelector(".main-display > span");
 numBtns = document.querySelectorAll(".num-btn");
 allClearBtn = document.querySelector(".ac-btn");
 backBtn = document.querySelector(".back-btn");
+deciPointBtn = document.querySelector(".deci-point-btn");
 
-// console.log(numBtns);
-// console.log(memoryDisplay);
-// console.log(mainDisplay);
-
-memoryDisplay.text = "0";
-mainDisplay.text = "0";
+resetAll();
 
 numBtns.forEach(btn => {
   btn.addEventListener("click", () => {
@@ -23,11 +27,20 @@ numBtns.forEach(btn => {
   })
 });
 
-allClearBtn.addEventListener("click", () => {
-  mainDisplay.textContent = "0";
+
+deciPointBtn.addEventListener("click", () => {
+  if (hasDeciPoint) return;
+  mainDisplay.textContent += ".";
+  hasDeciPoint = true;
 });
 
+allClearBtn.addEventListener("click", resetAll);
+
 backBtn.addEventListener("click", () => {
+  console.log();
+  if (mainDisplay.textContent.at(-1)) {
+    hasDeciPoint = false;
+  }
   mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
   if (!mainDisplay.textContent) {
     mainDisplay.textContent = 0;
