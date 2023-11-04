c37c01ba37a9afe42ec49cf7f2019f9fa94829fa
SiegfredLorelle <siegfredlorelle09@gmail.com>
2023-11-05 01:41:31 +0800

    Slide nav on menu btn click

--- js/responsiveness.js
+++ js/responsiveness.js
@@ -1,21 +1,46 @@
 const aside = document.querySelector("aside");
+const menuBtns = document.querySelectorAll(".menu-btn");
 
 const changeLayout = () => {
   console.log("RESIZING!");
 
   if (window.innerWidth > window.innerHeight) {
     console.log("DESKTOP");
-    aside.classList.add("slide-to-right");
-    aside.classList.remove("slide-to-left");
-
+    showMenu();
     aside.classList.add("desktop");
     return;
   }
   console.log("MOBILE");
-  aside.classList.add("slide-to-left");
-  aside.classList.remove("slide-to-right");
+  hideMenu();
   aside.classList.remove("desktop");
 }
+
+
+const showMenu = () => {
+  aside.classList.add("slide-to-right");
+  aside.classList.remove("slide-to-left");
+};
+
+const hideMenu = () => {
+  aside.classList.add("slide-to-left");
+  aside.classList.remove("slide-to-right");
+}
+
+menuBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    aside.classList.contains("slide-to-left") ? showMenu() : hideMenu();
+    console.log("CLICKED");
+  });
+
+})
+
+
+
+
+
+
+
+
 
 window.addEventListener("resize", changeLayout);
 changeLayout();
